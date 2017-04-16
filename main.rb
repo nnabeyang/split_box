a37d777b2f28f51bc4bc29b12ce68b1d2712706f
Noriaki Watanabe <nabeyang@gmail.com>
2017-04-16 16:31:27 +0900

    ソースコードに対応する設計図を生成するオプションを追加
    ./main.rb -v <filepath> <filepath2> …
    でimagesディレクトリにソースコードに対応する設計図が書き込まれる。
    複数のファイルパスに対応するので、次のようにすることで、全てのソースコードに対応する
    設計図がpng形式で書き込まれる。
    ./main.rb -v ./resources/*.sb

--- main.rb
+++ main.rb
@@ -5,12 +5,28 @@ require './nfa.rb'
 
 def main
  p = Parser.new
- src = open(ARGV[0]).read
- p.parse(src)
- post = reg2post(p.regex)
- s = compile(post)
- m = Machine.new(s)
- puts m.exec(ARGV[1]).inspect
+  if ARGV[0] == "-v"
+    ARGV.shift
+    white_graphs
+  else
+    src = open(ARGV[0]).read
+    p.parse(src)
+    post = reg2post(p.regex)
+    s = compile(post)
+    m = Machine.new(s)
+    puts m.exec(ARGV[1]).inspect
+  end
+end
+def white_graphs 
+  Dir.mkdir('./images') unless Dir.exist?("./images")
+  ARGV.each do|path|
+    p = Parser.new
+    p.parse(open(path).read)
+    fn = File.basename(path, '.sb')
+    io = IO.popen("dot -Tpng -o ./images/#{fn}.png", "r+")
+    io.puts p.dotfile
+    io.close_write
+  end
 end
 
 main
