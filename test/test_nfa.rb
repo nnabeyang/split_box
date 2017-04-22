2ba3095ca5bbdc88b0706e385830a5aea0cabc8f
Noriaki Watanabe <nabeyang@gmail.com>
2017-04-22 16:33:51 +0900

    nfa.rbのテストを追加

---
+++ test/test_nfa.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+require './nfa.rb'
+require 'minitest/autorun'
+
+class MainTest < MiniTest::Test
+  def test_default
+    s = compile("10->")
+    assert_equal("0", s.exec(0,"1"))
+    m = Machine.new(s)
+    assert_equal("0", m.exec("1"))
+  end
+
+  def test_dot
+    s = compile("10->10->\\.")
+    m = Machine.new(s)
+    assert_equal("00", m.exec("11"))
+  end
+
+  def test_star
+    s = compile("10->\\*  <-\\.")
+    m = Machine.new(s)
+    assert_equal("0000 ", m.exec("1111 "))
+    s = compile(".|->\\*  <-\\.")
+    m = Machine.new(s)
+    assert_equal("|||| ", m.exec(".... "))
+  end
+ 
+  def test_alt
+    s = compile("10->01->\\|")
+    m = Machine.new(s)
+    assert_equal("0", m.exec("1"))
+    assert_equal("1", m.exec("0"))
+  end
+end
